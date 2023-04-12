35fd1fdf9ef615d84118d78dbd41a1c471671a06
Asperius <banansmth@gmail.com>
2023-04-12 13:42:29 +0300

    fix: Поправить JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,35 @@
 let openPopupEditBtn = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseBtn = document.querySelector('.popup__close-button');
+// Находим значение Имени и Профессии на странице 
+let getProfileName = document.querySelector('.profile__name');
+let getProfileProfession = document.querySelector('.profile__profession');
+// Находим поля формы в DOM заменяя их значениями Имени и Профессии
+let nameInput = document.querySelector('.popup__input-item_profile_name');
+let jobInput = document.querySelector('.popup__input-item_profile_profission');
+// Выберем элементы, куда должны быть вставлены значения полей
+let putProfileName = document.querySelector('.profile__name');
+let putProfileProfession = document.querySelector('.profile__profession');
 
+// Функция открытия попапа
 function openPopup() {
   popup.classList.add('popup_opened');
-  // Находим значение Имени и Профессии на странице 
-let getProfileName = document.querySelector('.profile__name').textContent;
-let getProfileProfession = document.querySelector('.profile__profession').textContent;
-
-// Находим поля формы в DOM заменяя их значениями Имени и Профессии
-let nameInput = document.querySelector('.popup__input-item_profile_name').value = getProfileName;
-let jobInput = document.querySelector('.popup__input-item_profile_profission').value = getProfileProfession;
+  nameInput.value = getProfileName.textContent;
+  jobInput.value = getProfileProfession.textContent;
 }
 
-function closePopup(evt) {
-  let isOverlay = evt.target.classList.contains('popup');
-  let isCloseBtn = evt.target.classList.contains('popup__close-button');
-  let isSaveBtn = evt.target.classList.contains('popup__save-button');
-
-  if (isOverlay || isCloseBtn || isSaveBtn) {
-    popup.classList.remove('popup_opened')
-  }
+// Функция закрытия попапа
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
 //Открываем и Закрываем попап
 openPopupEditBtn.addEventListener('click', openPopup);
-popup.addEventListener('click', closePopup);
+popup.addEventListener('mousedown', (evt) => {
+  if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+    closePopup();
+  }
+});
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__input');
@@ -34,21 +38,15 @@ let formElement = document.querySelector('.popup__input');
 // она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Получим значение полей jobInput и nameInput из свойства value
-  nameInput = document.querySelector('.popup__input-item_profile_name').value;
-  jobInput = document.querySelector('.popup__input-item_profile_profission').value;
-  // Выберем элементы, куда должны быть вставлены значения полей
-  let putProfileName = document.querySelector('.profile__name');
-  let putProfileProfession = document.querySelector('.profile__profession');
   // Вставим новые значения с помощью textContent
-  putProfileName.textContent = nameInput;  
-  putProfileProfession.textContent = jobInput;
-  // Вроде все работало без вызова этой функции, но добавил ее по наставлению ревьюера
+  putProfileName.textContent = nameInput.value;
+  putProfileProfession.textContent = jobInput.value;
+
   closePopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
 
 
