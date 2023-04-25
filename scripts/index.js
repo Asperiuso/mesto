6db87150cd2d474319972af11acb5e388d5b6954
Asperius <banansmth@gmail.com>
2023-04-25 18:07:05 +0300

    fix: Внести все правки по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,77 +1,68 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+import initialCards from './db.js';
 
 // Находим значение Имени и Профессии на странице 
-const getProfileName = document.querySelector('.profile__name');
-const getProfileProfession = document.querySelector('.profile__profession');
+const inputProfileName = document.querySelector('.profile__name');
+const inputProfileProfession = document.querySelector('.profile__profession');
 // Находим поля формы в DOM 
-let nameInput = document.querySelector('.popup__input-item_profile_name');
-let jobInput = document.querySelector('.popup__input-item_profile_profission');
-let CardTitleInput = document.querySelector('.popup__input-item_card_title');
-let CardLinkInput = document.querySelector('.popup__input-item_card_link');
+const formInputName = document.querySelector('.popup__input-item_profile_name');
+const formInputJob = document.querySelector('.popup__input-item_profile_profission');
+const titleInputCard = document.querySelector('.popup__input-item_card_title');
+const linkInputCard = document.querySelector('.popup__input-item_card_link');
 // Выберем элементы, куда должны быть вставлены значения полей
-let putProfileName = document.querySelector('.profile__name');
-let putProfileProfession = document.querySelector('.profile__profession');
-const popupCard = document.querySelector('.popup-card');
-const editBtn = document.querySelector('.profile__edit-button');
-const addCard = document.querySelector('.profile__add-button');
+const popupInputCard = document.querySelector('.popup-card');
+const btnEdit = document.querySelector('.profile__edit-button');
+const btnAdd = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup-add');
 const popupEdit = document.querySelector('.popup-edit');
-const popupCloseBtn = document.querySelectorAll('.popup__close-button');
-const popups = document.querySelectorAll('.popup');
 // Находим форму в DOM
-let formProfile = document.querySelector('#editProfile');
-let formCard = document.querySelector('#editCard');
+const formProfile = document.querySelector('#editProfile');
+const formCard = document.querySelector('#editCard');
 const area = document.querySelector('.area')
+//Модальные константы
+const modalImg = popupInputCard.querySelector('img');
+const modalText = popupInputCard.querySelector('.popup__card-title');
+//Переменная попапа
+const popups = document.querySelectorAll('.popup');
+const popupsElement = Array.from(popups);
 
 //Функция закрытия попапа
-function closePopup(popup) {
-  popup.classList.remove('popup_opened')
+function closePopup(popups) {
+  popups.classList.remove('popup_opened')
 }
 
+// Функция открытия попапа
+function openPopup(popups) {
+  popups.classList.add('popup_opened');
+}
+
+popupsElement.forEach(el => {
+  el.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+      closePopup(el);
+    }
+  });
+});
+
 //Функция добавления элементов
-function addCards(name, link)  {
+function addCards(name, link) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = link;
   cardElement.querySelector('.card__image').alt = name;
   cardElement.querySelector('.card__title').textContent = name;
   cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('card__like-button_active');
+    evt.target.classList.toggle('card__like-button_active');
   });
   cardElement.querySelector('.card__del-button').addEventListener('click', function (evt) {
-      (evt.target.closest('.card')).remove();
+    (evt.target.closest('.card')).remove();
   });
 
-    cardElement.querySelector('.card__image').addEventListener('click', function (evt) {
-    popupCard.querySelector('img').src = evt.target.src;
-    popupCard.querySelector('.popup__card-title').textContent = evt.target.alt;
-    openPopup(popupCard);
-});
+  cardElement.querySelector('.card__image').addEventListener('click', function (evt) {
+    modalImg.src = evt.target.src;
+    modalImg.alt = evt.target.alt;
+    modalText.textContent = evt.target.alt;
+    openPopup(popupInputCard);
+  });
 
   return cardElement;
 
@@ -81,45 +72,34 @@ initialCards.forEach(function (el) {
   area.insertAdjacentElement('beforeend', addCards(el.name, el.link))
 });
 
-// Функция открытия попапа
-function openPopup(popup) {
-  popup.addEventListener('mousedown', (evt) => {
-    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
-      closePopup(popup);
-    }
-  });
-  nameInput.value = getProfileName.textContent;
-  jobInput.value = getProfileProfession.textContent;
-  CardTitleInput.value = '';
-  CardLinkInput.value = '';
-  popup.classList.add('popup_opened');
-}
-
 // Обработчик «отправки» форм
-function handleFormSubmit(evt) {
+function submitFormEdit(evt) {
   evt.preventDefault();
-  putProfileName.textContent = nameInput.value;
-  putProfileProfession.textContent = jobInput.value;
+  inputProfileName.textContent = formInputName.value;
+  inputProfileProfession.textContent = formInputJob.value;
   closePopup(popupEdit);
 }
+// Прикрепляем обработчик к форме:
+formProfile.addEventListener('submit', submitFormEdit);
 
-function formCardSubmit(evt) {
+// Обработчик для добавления карточек
+function submitFormAdd(evt) {
   evt.preventDefault();
-  area.insertAdjacentElement('afterbegin', addCards(CardTitleInput.value, CardLinkInput.value));
+  area.insertAdjacentElement('afterbegin', addCards(titleInputCard.value, linkInputCard.value));
   closePopup(popupAdd);
 }
+formCard.addEventListener('submit', submitFormAdd);
 
-editBtn.addEventListener('click', function () {
+btnEdit.addEventListener('click', function () {
+  formInputName.value = inputProfileName.textContent;
+  formInputJob.value = inputProfileProfession.textContent;
   openPopup(popupEdit);
 });
 
-addCard.addEventListener('click', function () {
+btnAdd.addEventListener('click', function () {
+  titleInputCard.value = '';
+  linkInputCard.value = '';
   openPopup(popupAdd);
 });
 
-// Прикрепляем обработчик к формам:
-
-formProfile.addEventListener('submit', handleFormSubmit);
-
-formCard.addEventListener('submit', formCardSubmit);
 
